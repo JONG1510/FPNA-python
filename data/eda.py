ee9d3fc0d8cda966b03b6ff82d4fd72eeb0d22d3
JONG15 <87769421+JONG1510@users.noreply.github.com>
2026-03-29 17:58:35 +0800

    Create eda.py

---
+++ data/eda.py
@@ -0,0 +1,16 @@
+# 2025 - 2026 gold rates 
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from statsmodels.graphics.tsaplots import plot_acf
+import statsmodels.api as sm
+import warnings
+
+url = "https://raw.githubusercontent.com/<username>/FPNA/main/data/SI_latest.csv"
+df = pd.read_csv(url)
