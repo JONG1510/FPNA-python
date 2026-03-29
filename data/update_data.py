e7a3686279d7df9038c759c4a91dc60917bfa19b
JONG15 <87769421+JONG1510@users.noreply.github.com>
2026-03-29 17:52:16 +0800

    Fetch 'Close' and 'Volume' prices from Yahoo Finance
    Modified data fetching to include both 'Close' and 'Volume' prices.

--- data/update_data.py
+++ data/update_data.py
@@ -8,7 +8,7 @@ def update_silver():
     
     # Fetch last 30 days of data
     # data = yf.download(tickers, period="1mo", interval="1d")['Close'] -- Only fetch Close prices
-    data = yf.download(tickers, period="1mo", interval="1d")
+    data = yf.download(tickers, period="1mo", interval="1d")['Close','Volume'] 
     
     # Clean and save
     data.to_csv('data/SI_latest.csv')
