f3834f48adef224a8fdfa48c4339807c584824ef
JONG15 <87769421+JONG1510@users.noreply.github.com>
2026-05-23 20:42:56 +0800

    Fetch silver data from specific start date to today

--- data/update_data.py
+++ data/update_data.py
@@ -1,14 +1,16 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
+from datetime import date
 
+today = date.today()
 def update_silver():
     # Define tickers: Silver (SI=F)
     tickers = ["SI=F"]
     
     # Fetch last 30 days of data
     # data = yf.download(tickers, period="1mo", interval="1d")['Close'] -- Only fetch Close prices
-    data = yf.download(tickers, period="1mo", interval="1d")[['Close','Volume']] 
+    data = yf.download(tickers, start="2025-10-02", end=today, interval="1d")[['Close','Volume']] 
     
     # Clean and save
     data.to_csv('data/SI_latest.csv')
