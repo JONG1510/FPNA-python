cd6fe13b9bff375681c6b6bf5090ce31874690b3
JONG15 <87769421+JONG1510@users.noreply.github.com>
2026-05-23 20:43:46 +0800

    Update update_data_gold.py

--- data/update_data_gold.py
+++ data/update_data_gold.py
@@ -1,6 +1,9 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
+from datetime import date
+
+today = date.today()
 
 def update_gold():
     # Define tickers: Gold (GC=F)
@@ -8,7 +11,7 @@ def update_gold():
     
     # Fetch last 30 days of data
     # data = yf.download(tickers, period="1mo", interval="1d")['Close'] -- Only fetch Close prices
-    data = yf.download(tickers, period="1mo", interval="1d")
+    data = yf.download(tickers, start="2025-10-02", end=today, interval="1d")
     
     # Clean and save
     data.to_csv('data/GC_latest.csv')
